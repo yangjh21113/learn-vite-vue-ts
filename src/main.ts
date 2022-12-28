06a06cb83d6d41e6c3f7874340d99309af5f2f5c
yangjihong <jihongyang@shannonai.com>
2022-12-28 18:10:46 +0800

    feat: 使用vue-router

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { createPinia } from 'pinia'
 import './style.css'
 import './common.scss'
 import App from './App.vue'
+import router from './router/index'
 import 'virtual:windi.css'
 import 'ant-design-vue/dist/antd.css'
 
 const app = createApp(App)
 app.use(createPinia())
+app.use(router)
 app.mount('#app')
 
