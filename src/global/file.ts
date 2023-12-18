e12b54213fa48d258a4083200b927acb310a5317
yangjihong <jihongyang@shannonai.com>
2023-12-18 18:46:05 +0800

    feat: 自定义上传多个文件

--- src/global/file.ts
+++ src/global/file.ts
@@ -28,3 +28,33 @@ export function webChooseFile(cb: (file: File) => unknown, accept?: string): voi
 	e.initEvent('click', false, true)
 	input.dispatchEvent(e)
 }
+
+export function webMulChooseFile(cb: (file: FileList) => unknown, accept?: string): void {
+	const id = 'file-selector-88888'
+	let input = document.getElementById(id) as HTMLInputElement
+	if (isNull(input)) {
+		input = document.createElement('input')
+		input.id = id
+		input.type = 'file'
+		input.multiple = true
+		input.style.position = 'fixed'
+		input.style.left = '-100000px'
+		document.body.appendChild(input)
+	} else {
+		input.value = ''
+	}
+	if (accept) {
+		input.accept = accept
+	} else {
+		input.removeAttribute('accept')
+	}
+	input.onchange = (): void => {
+		if (input.files?.length) {
+			cb(input.files)
+		}
+	}
+	input.classList.add('selectFile')
+	const e = document.createEvent('MouseEvent') as unknown as Event
+	e.initEvent('click', false, true)
+	input.dispatchEvent(e)
+}
