6e35aae6a28d67be6430c5983746d3f6ffda9578
yangjihong <jihongyang@shannonai.com>
2023-12-18 18:57:53 +0800

    feat: 下载文件

--- src/global/file.ts
+++ src/global/file.ts
@@ -58,3 +58,27 @@ export function webMulChooseFile(cb: (file: FileList) => unknown, accept?: strin
 	e.initEvent('click', false, true)
 	input.dispatchEvent(e)
 }
+
+export function webDownloadFileByUrl(url: string, name: string): void {
+	const xhr = new XMLHttpRequest()
+	xhr.open('GET', url, true)
+	xhr.responseType = 'blob'
+	xhr.onload = (): void => {
+		if (xhr.status === 200) {
+			const a = document.createElement('a')
+			a.download = name
+			a.href = URL.createObjectURL(xhr.response)
+			a.click()
+		}
+	}
+	xhr.send()
+}
+
+export function downloadToTxt(text: string, filename: string) {
+	const element = document.createElement('a')
+	element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text))
+	element.setAttribute('download', filename)
+	element.style.display = 'none'
+	document.body.appendChild(element)
+	element.click()
+}
