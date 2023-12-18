c67e40e21e084fa27212c3c5dc576da9761e505c
yangjihong <jihongyang@shannonai.com>
2023-12-18 12:28:12 +0800

    feat: 测试一下非静态函数

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <script setup lang="ts">
+import { APIs } from './service/apiList'
 import AxiosRequestError from './service/error'
 import $api from './service/webRequest'
 
 // 请求API类的静态函数
-$api.getUserInfo()
+//$api.getUserInfo()
+//	.then(() => {
+//		//
+//	})
+//	.catch((err: AxiosRequestError) => {
+//		// 使用console.dir()方法，可以在控制台中显示指定JavaScript对象的属性，并通过类似文件树样式的交互列表显示
+//		// 所以别老用 console.log了
+//		console.dir(err)
+//	})
+
+$api.get(APIs.login)
 	.then(() => {
 		//
 	})
 	.catch((err: AxiosRequestError) => {
-		// 使用console.dir()方法，可以在控制台中显示指定JavaScript对象的属性，并通过类似文件树样式的交互列表显示
-		// 所以别老用 console.log了
 		console.dir(err)
 	})
 </script>
