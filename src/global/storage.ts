f5116cc933485b053f95d5225d5e190646202e85
yangjihong <jihongyang@shannonai.com>
2023-12-18 17:36:42 +0800

    feat: 封装 localStorage

---
+++ src/global/storage.ts
@@ -0,0 +1,56 @@
+// 所有 key 用常量存储起来
+export enum StorageKeys {
+	TOKEN = 'TOKEN',
+	NAME_LIST = 'NAME_LIST',
+}
+
+export function setStorage(key: string, val: string): void {
+	localStorage.setItem(key, val)
+}
+
+export function getStorage(key: string): string | null {
+	return localStorage.getItem(key) || ''
+}
+
+export function removeStorage(key: string): void {
+	localStorage.removeItem(key)
+}
+
+// 判断是否存在
+export function hasStorage(key: string): boolean {
+	return getStorage(key) !== null
+}
+// 取数组类型的，直接返回解析后的数组
+export function getStorageArray(key: string): Array<unknown> | null {
+	const data = localStorage.getItem(key)
+	if (data === null) {
+		return data
+	}
+	try {
+		const arr = JSON.parse(data)
+		if (Array.isArray(arr)) {
+			return arr
+		}
+		localStorage.removeItem(key)
+	} catch (err) {
+		console.log('json parse error getStorageArray', err)
+	}
+	return null
+}
+// 取对象类型的，直接返回解析后的对象
+export function getStorageObject(key: string): Object | null {
+	const data = localStorage.getItem(key)
+	if (data === null) {
+		return data
+	}
+	try {
+		const obj = JSON.parse(data)
+		if (Object.prototype.toString.call(obj) === '[object Object]') {
+			return obj
+		}
+		localStorage.removeItem(key)
+	} catch (err) {
+		console.log('json parse error getStorageObject', err)
+	}
+	return null
+}
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -4,16 +4,29 @@
 	<h1>{{ $t('name') }}</h1>
 	<button @click="$i18n.locale = 'zh'">中文</button>
 	<button @click="$i18n.locale = 'en'">英文</button>
+	<br />
+	<button @click="savaArray">存一个数组</button>
+
+	<br />
+	<button @click="getArray">取一个数组</button>
 	<router-view></router-view>
 </template>
 <script lang="ts" setup>
 import { useRoute, useRouter } from 'vue-router'
 import SvgIcon from '@/components/SvgIcon.vue'
-
+import { setStorage, getStorageArray, StorageKeys } from '@/global/storage'
 const router = useRouter()
 function toAccount() {
 	router.push({
 		name: 'account',
 	})
+}
+
+function savaArray() {
+	setStorage(StorageKeys.NAME_LIST, JSON.stringify(['小明', '小红']))
+}
+function getArray() {
+	const list = getStorageArray(StorageKeys.NAME_LIST)
+	console.log(list)
 }
 </script>
