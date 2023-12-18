3421a55b0b2ecbf6e33d7b348bc87e5e90864714
yangjihong <jihongyang@shannonai.com>
2023-12-18 15:53:18 +0800

    feat: 使用 i18n 多语言

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,10 @@ import 'virtual:windi.css'
 import 'ant-design-vue/dist/antd.css'
 import '@/polyfill/polyfill'
 import 'virtual:svg-icons-register'
+import i18n from '@/global/i18n'
 
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
+app.use(i18n)
 app.mount('#app')
