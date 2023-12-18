54814ea46429edf50e972711c0d7deb7c4796250
yangjihong <jihongyang@shannonai.com>
2023-12-18 10:57:08 +0800

    feat: 应用handleError

--- src/service/request.ts
+++ src/service/request.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosInstance } from 'axios'
+import { handleError } from './handleError'
 
 function createRequestInstance(url: string): AxiosInstance {
 	const instance = axios.create({
@@ -6,6 +7,16 @@ function createRequestInstance(url: string): AxiosInstance {
 		withCredentials: true, // 允许跨域携带cookie
 		baseURL: url, // 请求地址
 	})
+
+	instance.interceptors.response.use(
+		async res => {
+			return res
+		},
+		async err => {
+			err = await handleError(err)
+			return Promise.reject(err)
+		},
+	)
 	return instance
 }
 
